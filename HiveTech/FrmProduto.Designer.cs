58554c7ac3ee1c65fc3aa44a4eba582479fe1332
Brendo Almeida <brendolee2004@outlook.com>
2022-11-23 17:01:29 -0300

    Criação do frmProdutos

---
+++ HiveTech/FrmProduto.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace HiveTech
+{
+    partial class FrmProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvProdutos = new System.Windows.Forms.DataGridView();
+            this.Img = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Add = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvProdutos
+            // 
+            this.DgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvProdutos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Img,
+            this.Nome,
+            this.Descricao,
+            this.Add});
+            this.DgvProdutos.Location = new System.Drawing.Point(8, 8);
+            this.DgvProdutos.Name = "DgvProdutos";
+            this.DgvProdutos.Size = new System.Drawing.Size(536, 336);
+            this.DgvProdutos.TabIndex = 0;
+            // 
+            // Img
+            // 
+            this.Img.HeaderText = "Img";
+            this.Img.Name = "Img";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Descricao
+            // 
+            this.Descricao.HeaderText = "Descrição";
+            this.Descricao.Name = "Descricao";
+            // 
+            // Add
+            // 
+            this.Add.HeaderText = " ";
+            this.Add.Name = "Add";
+            // 
+            // FrmProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 355);
+            this.Controls.Add(this.DgvProdutos);
+            this.Name = "FrmProduto";
+            this.Text = "FrmProduto";
+            this.Load += new System.EventHandler(this.FrmProduto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvProdutos;
+        private System.Windows.Forms.DataGridViewImageColumn Img;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Add;
+    }
+}
---
+++ HiveTech/FrmProduto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiveTech
+{
+    public partial class FrmProduto : Form
+    {
+        ProdutoDAO ProdDao = new ProdutoDAO();
+
+        public FrmProduto()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmProduto_Load(object sender, EventArgs e)
+        {
+            List<Produto> ListProduto = ProdDao.Listar();
+
+            DataGridViewImageColumn img = new DataGridViewImageColumn();
+            Image image = Image.FromFile("../image/imgTeste.png");
+            img.Image = image;
+            DgvProdutos.Columns.Add(img);
+            img.HeaderText = "Image";
+            img.Name = "img";
+
+            // this.DgvProdutos.Rows.Add(img, "a", "a", "a");
+        }
+    }
+}
